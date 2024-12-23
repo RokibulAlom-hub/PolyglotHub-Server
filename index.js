e8c804651344e03bac9a3b7ff35c4d53df01ac8b
Rokibul Alom <itismehey975@gmail.com>
2024-12-23 12:36:20 +0600

    to create tutorials and geting it from server through email query is done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 7000
 const app = express();
@@ -8,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jds8f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +27,21 @@ async function run() {
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const tutorialsColloction =  client.db('LanguagePortal').collection('alltutorials');
+
+    // create tutorials
+    app.post('/add-tutorials',async(req,res) => {
+      const tutorials = req.body;
+      const result = await tutorialsColloction.insertOne(tutorials)
+      res.send(result)
+    })
+    // getting tutorials by 
+    app.get('/getTutorials',async(req,res)=>{
+      const email = req.query.email;
+      const query = {email: email};
+      const result =await tutorialsColloction.find(query).toArray();
+      res.send(result)
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
